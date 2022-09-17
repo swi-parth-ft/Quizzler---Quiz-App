53c94d98f765755678076d786a3f41ad40d155da
Parth Antala <52197140+parallel-appy@users.noreply.github.com>
2022-09-17 12:25:36 -0400

    final

--- Quizzler/ViewController.swift
+++ Quizzler/ViewController.swift
@@ -1,19 +1,87 @@
 //
 //  ViewController.swift
-//  Quizzler
+//  Quizzler-iOS13
 //
-//  Created by Parth Antala on 2022-09-16.
+//  Created by Angela Yu on 12/07/2019.
+//  Copyright © 2019 The App Brewery. All rights reserved.
 //
 
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var questionLabel: UILabel!
+    @IBOutlet weak var progressBar: UIProgressView!
+    @IBOutlet weak var option2: UIButton!
+   
+    @IBOutlet weak var option1: UIButton!
+    @IBOutlet weak var option3: UIButton!
+    @IBOutlet weak var scoreLabel: UILabel!
+    
+    var quizBrain = QuizBrain()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        updateUI()
     }
 
+    @IBAction func answerButtonPressed(_ sender: UIButton) {
+        
+        let userAnswer = sender.currentTitle!
+        
+        let userGotItRight = quizBrain.checkAnswer(userAnswer: userAnswer)
+        
+        if userGotItRight {
+            sender.backgroundColor = UIColor.green
+        } else {
+            sender.backgroundColor = UIColor.red
+        }
+        
+        quizBrain.nextQuestion()
+        
+        Timer.scheduledTimer(timeInterval: 0.2, target: self, selector: #selector(updateUI), userInfo: nil, repeats: false)
+    }
+    
+    @objc func updateUI() {
+        questionLabel.text = quizBrain.getQuestionText()
+        progressBar.progress = quizBrain.getProgress()
+        scoreLabel.text = "Score: \(quizBrain.getScore())"
+        
+        option1.backgroundColor = UIColor.clear
+        option2.backgroundColor = UIColor.clear
+        option3.backgroundColor = UIColor.clear
+        
+        let options = quizBrain.getOptions()
+        option1.setTitle(options[0], for: .normal)
+        option2.setTitle(options[1], for: .normal)
+        option3.setTitle(options[2], for: .normal)
+    }
 
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
